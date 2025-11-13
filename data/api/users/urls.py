cb09e5deba87f112936c41230105fd08dad664fe
xo0102 <63778661+xo0102@users.noreply.github.com>
2025-11-13 17:52:47 +0900

    [CREATE] 마이페이지 기능 구현

--- data/api/users/urls.py
+++ data/api/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .register import register_api
+from . import mypage
 
 urlpatterns = [
     path('api/register/', register_api, name='api-register'),
+    path("mypage/", mypage.mypage_view, name="mypage"),
 ]
