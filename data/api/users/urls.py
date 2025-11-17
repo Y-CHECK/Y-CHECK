ea9d8c0ba4daf7c4264dd922d7ce5d0c15a9e9b5
xo0102 <63778661+xo0102@users.noreply.github.com>
2025-11-18 02:40:52 +0900

    [CREATE] 로그인 기능 구현

--- data/api/users/urls.py
+++ data/api/users/urls.py
@@ -1,8 +1,8 @@
-# users/urls.py
-
 from django.urls import path
 from .register import register_api
+from .login import login_api   # 추가!!
 
 urlpatterns = [
-    path('api/register/', register_api, name="api-register"),
+    path('api/register/', register_api, name='api-register'),
+    path('api/login/', login_api, name='api-login'),  # 추가된 라우트
 ]
