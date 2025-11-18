eaf6bb5cabaad1be4e24ac95d5821fcbcc3dd16e
xo0102 <63778661+xo0102@users.noreply.github.com>
2025-11-18 21:24:50 +0900

    [FIX] 마이페이지 로그인 유지

--- data/api/users/urls.py
+++ data/api/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .register import register_api
-from .login import login_api   # 추가!!
+from .login import login_api
+from .mypage import mypage_api   # ✅ 방금 만든 함수
 
 urlpatterns = [
-    path('api/register/', register_api, name='api-register'),
-    path('api/login/', login_api, name='api-login'),  # 추가된 라우트
+    path("api/register/", register_api, name="api-register"),
+    path("api/login/", login_api, name="api-login"),
+    path("api/mypage/", mypage_api, name="api-mypage"),  # ✅ 마이페이지 API
 ]
