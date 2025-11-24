fa97ff6c608439b04c84e21df22b5b5683d031b4
xo0102 <63778661+xo0102@users.noreply.github.com>
2025-11-24 13:04:29 +0900

    [UPDATE] 계산기 기능 보충

--- data/api/curriculum/urls.py
+++ data/api/curriculum/urls.py
@@ -1,8 +1,10 @@
-# curriculum/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     # POST /api/curriculum/calculate/
     path("calculate/", views.calculate_graduation, name="calculate_graduation"),
+
+    # GET /api/curriculum/courses/
+    path("courses/", views.get_courses, name="get_courses"),
 ]
