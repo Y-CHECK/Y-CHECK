39d1c76ad77582dc3cd17dacbe829cc965905781
Junwoo Choi <chltkddud03@naver.com>
2025-11-25 16:31:22 +0900

    [UPDATE] 로그아웃 기능 추가

--- data/api/users/urls.py
+++ data/api/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .register import register_api
 from .login import login_api
-from .mypage import mypage_api   # ✅ 방금 만든 함수
+from .mypage import mypage_api
+from .logout import logout_api
 
 urlpatterns = [
     path("api/register/", register_api, name="api-register"),
     path("api/login/", login_api, name="api-login"),
-    path("api/mypage/", mypage_api, name="api-mypage"),  # ✅ 마이페이지 API
+    path("api/mypage/", mypage_api, name="api-mypage"),
+    path("api/logout/", logout_api, name="api-logout"),  # ← 통일!
 ]
