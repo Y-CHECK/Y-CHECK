2d18e50e660aaad2197da22e499503eaf1e6a220
김태훈 <xo0102@naver.com>
2025-11-28 22:13:03 +0900

    [FIX] 프로필 카드 오류 수정

--- data/api/config/settings.py
+++ data/api/config/settings.py
@@ -35,8 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'users',
+    'users.apps.UsersConfig',
     'curriculum',
 ]
 
