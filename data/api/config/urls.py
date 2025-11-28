3162d28ef70f06091c34c80ebe0088aceebda430
woo.__.bee <72401646+Ranunculus2165@users.noreply.github.com>
2025-11-28 17:33:49 +0900

    Revert "Mainpage1"

--- data/api/config/urls.py
+++ data/api/config/urls.py
@@ -1,23 +1,16 @@
 """
 URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    # --------------------------
+    # 기존 백엔드 기능 유지
+    # --------------------------
     path('admin/', admin.site.urls),
     path('api/curriculum/', include('curriculum.urls')),
     path('', include('users.urls')),   # 로그인, 회원가입 등 그대로 유지
@@ -45,8 +38,4 @@ urlpatterns = [
 
     # 졸업요건 계산기
     path('calculator/', TemplateView.as_view(template_name="calculator.html"), name="calculator"),
-
-    path('', include('users.urls')),  # /api/register/ 같은 URL 여기로 연결
-    path("api/timetable/", include("timetable.urls")),
-    path("api/footprints/", include("footprints.urls")),
 ]
